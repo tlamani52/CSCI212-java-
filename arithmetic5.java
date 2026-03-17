b262227e0ff58cca3c6a4f63c689d5cb552d85e3
Angel Tlamani <143969118+tlamani52@users.noreply.github.com>
2026-03-17 15:51:08 -0400

    Add files via upload

---
+++ arithmetic5.java
@@ -0,0 +1,32 @@
+public class arithmetic5 {
+    public static void main(String[] args){
+        double x = 10, y = 3, z;
+        
+        //z = x + y; 
+        //z = x - y;
+        //z = x * y;
+        //z = x / y;
+        //z = x % y;
+        
+        //augmented assignment varaible 
+        //x = x + y;
+        //x += y;
+        //x = x - y;
+        //x -= y;
+        //x = x * y;
+        //x *= y;
+        //x = x / y;
+        //x /= y;
+        //x = x % y;
+        //x %= y;
+        //incrementing and decrement operators 
+        //x++;
+        //x--;
+        System.out.println(x);
+
+        
+        
+
+
+    }
+}
